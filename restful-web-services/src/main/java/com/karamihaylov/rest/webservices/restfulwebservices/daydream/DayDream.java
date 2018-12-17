c9dff49ba54a4126940ec80c38d43ebb69502502
Georgi Karamihaylov <karamihaylov@ifao.net>
2018-12-17 14:44:30 +0200

    updated swagger

--- restful-web-services/src/main/java/com/karamihaylov/rest/webservices/restfulwebservices/daydream/DayDream.java
+++ restful-web-services/src/main/java/com/karamihaylov/rest/webservices/restfulwebservices/daydream/DayDream.java
@@ -1,14 +1,42 @@
 package com.karamihaylov.rest.webservices.restfulwebservices.daydream;
 
+
+import java.util.Date;
+
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description="All details about the DayDream")
 public class DayDream {
 
 	private Integer ID;
 	
 	@Size(min=2, message="Name should have at lease 2 letters")
+	@ApiModelProperty(notes="Name should have at lease 2 letters")
 	private String name;
 	
+	@ApiModelProperty(notes="Day of Creation of Dream")
+	private Date createDate;
+	
+
+	public DayDream(Integer iD, @Size(min = 2, message = "Name should have at lease 2 letters") String name,
+			Date createDate) {
+		super();
+		ID = iD;
+		this.name = name;
+		this.createDate = createDate;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
 	protected DayDream() {
 	}
 	
@@ -16,6 +44,7 @@ public class DayDream {
 		super();
 		this.name = name;
 		this.ID = ID;
+
 	}
 
 	public Integer getID() {
